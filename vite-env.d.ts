593ca422232151dcecfc9dbea98e224ec574657a
sw326 <sungwoo326@gmail.com>
2024-08-25 23:05:02 +0900

    feat :  kakao login

--- vite-env.d.ts
+++ vite-env.d.ts
@@ -4,6 +4,9 @@ interface ImportMetaEnv {
   readonly VITE_APP_TITLE: string;
   readonly VITE_API_URL: string;
   readonly VITE_PARTNER_API_URL: string;
+
+  VITE_KAKAO_REST_API_KEY: string;
+  VITE_KAKAO_REDIRECT_URI: string;
 }
 
 interface ImportMeta {
